f1a9f07085905b1fccb2e6eabe1238568d199b2b
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-03-26 19:17:46 +0530

    updated

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import './Cart.css';
 
-function Cart({techStack,descrip,src,deploy,github}) {
+function Cart({projectName,techStack,descrip,src,deploy,github}) {
+    console.log("techStack  ",techStack)
+    // let iconData = [
+    //     {
+    //         src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+    //     },
+    //     {
+    //         src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+    //     },
+    //     {
+    //         src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+    //     },
+    //     {
+    //         src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+    //     },
+    //     {
+    //         src :"./image/express-modified.png"
+    //     },
+    //     {
+    //         src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+    //     },
+    //     {
+    //         src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+    //     }
+    // ]
     return (
             <div className="card card_container">
                 <img src={src} className="card-img-top card_img" alt="project image"/>
                     <div className="card-body card_body_container">
-                        <p className="card-text card_text_container" >{descrip}</p>
+                        <h3 className='project__name'>{projectName}</h3>
+                        <p className="card-text card_text_container projectDesc" >{descrip}</p>
                         {/* <p className='techStack'>{techStack}</p> */}
                         <div className="techStack_logo">
-                            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="" className='teckStack_icon'/>
-                            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="" className='teckStack_icon'/>
-                            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="" className='teckStack_icon'/>
+                            <img src={techStack[0].src} alt="" className='teckStack_icon'/>
+                            <img src={techStack[1].src} alt="" className='teckStack_icon'/>
+                            <img src={techStack[2].src} alt="" className='teckStack_icon'/>
                             {/* <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="" className='teckStack_icon'/> */}
-
+                            {/* <p className='teckStack__logo'>{techStack}</p> */}
                         </div>
                     </div>
                     <div className='btn_container'>
--- src/Project.jsx
+++ src/Project.jsx
@@ -8,13 +8,43 @@ function Project() {
             <h1>Projects</h1>
             <hr className='' />
             <div className='project_container'>
-                <Cart src={'image/pulsePlus_bg2.png'} techStack={`HTML | css | Basic Javascript`} descrip={`Pulse plus is a pharmacuitical e-commerce website where customers can easily get the medicine they need at very low prices and they can also book consultation from certified doctor.`} deploy={`https://61ff9721624257b707b4e751--pulsepluspharmacy.netlify.app/`} github={`https://github.com/Rohanverma4/UNIT_2_PROJECT_TESTING`}/>
+                <Cart src={'image/swiggy-bg.png'} projectName={'Swiggy Clone'} techStack={[
+                    {
+                        src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+                    },
+                    {
+                        src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+                    },
+                    {
+                        src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+                    }
+                ]} descrip={`Swiggy is Indian online food ordering and delivery platform.`} deploy={`https://masaiswiggyclone.herokuapp.com/`} github={`https://github.com/swaraj54/swiggy-clone`} />
 
 
-                <Cart src={'image/homeDepot_bg.png'} techStack={`HTML | css | Advanced Javascript`} descrip={`.The Home Depot, Inc., commonly known as Home Depot, is the largest home improvement retailer in the United States, supplying tools, construction products, appliances, and services. `} deploy={`https://shyamal90.github.io/homeDepot_clone/`} github={`https://github.com/Shyamal90/homeDepot_clone`}/>
+                <Cart src={'image/homeDepot_bg.png'} projectName={'Home Depot Clone'} techStack={[
+                        {
+                            src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                        },
+                        {
+                            src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                        },
+                        {
+                            src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                        }
+                    ]} descrip={`.The Home Depot, Inc., commonly known as Home Depot, is the largest home improvement retailer in the United States. `} deploy={`https://shyamal90.github.io/homeDepot_clone/`} github={`https://github.com/Shyamal90/homeDepot_clone`} />
 
-                
-                <Cart src={'image/pulsePlus_bg.png'} techStack={`HTML | css | Basic Javascript`} descrip={`Pulse plus is a pharmacuitical e-commerce website where customers can easily get the medicine they need at very low prices and they can also book consultation from certified doctor.`} deploy={`https://61ff9721624257b707b4e751--pulsepluspharmacy.netlify.app/`} github={`https://github.com/Rohanverma4/UNIT_2_PROJECT_TESTING`}/>
+
+                    <Cart src={'image/pulsePlus_bg.png'} projectName={'Pulse Plus Clone'} techStack={[
+                        {
+                            src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                        },
+                        {
+                            src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                        },
+                        {
+                            src :"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                        }
+                    ]} descrip={`Pulse plus is a pharmacuitical e-commerce website where customers can easily get the medicine .`} deploy={`https://61ff9721624257b707b4e751--pulsepluspharmacy.netlify.app/`} github={`https://github.com/Rohanverma4/UNIT_2_PROJECT_TESTING`} />
             </div>
         </div>
 
